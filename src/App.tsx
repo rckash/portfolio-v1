4fdaa88f92f9b9a9993339e388cf69a4c86b4325
rckash <rckashyapcpe@gmail.com>
2024-05-31 16:14:48 +0800

    Change UI color and add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Button } from "./components/Button";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <h1 className="App">Hello World!</h1>;
-      <Button />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hero.css";
+
+export const Hero = () => {
+  return (
+    <>
+      <section className="hero-section">
+        <h1 className="hero-text">I'm rirru</h1>
+        <h2 className="hero-subtext">
+          Tech-driven problem-solver with a passion for building innovative apps
+          and websites that people love. Thrives on continuous learning and
+          enjoys the challenge of creating impactful software.
+        </h2>
+      </section>
+    </>
+  );
+};
